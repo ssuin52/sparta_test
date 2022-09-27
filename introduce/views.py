f1f6b08cd6e4bb19cb17ae45df1f6059a3bc3680
suin <parksuin55@gmail.com>
2022-09-27 09:57:08 +0900

    second commit

--- introduce/views.py
+++ introduce/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from .models import AccessLog
+
 
 def introduce_view(request):
-    return render(request, 'index.html')
+    if request.method == 'POST':
+        create_at = request.POST.get('create_at',None)
+        location = request.POST.get('location',None)
+
+        new_log = AccessLog()
+        new_log.created_at = create_at
+        new_log.location = location
+        new_log.save()
+        
+    elif request.method == 'GET':
+        return render(request, 'index.html')
