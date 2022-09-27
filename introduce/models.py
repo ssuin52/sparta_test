d6b2e23af64cd02d9e9129ec09846fc15d46280a
suin <parksuin55@gmail.com>
2022-09-27 11:13:47 +0900

    second correct

--- introduce/models.py
+++ introduce/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class AccessLog(models.Model):
-    class Meta:
-        db_table = "access_log"
+    created_at = models.DateTimeField("접속 시간", auto_now_add=True)
+    location = models.CharField("접속 경로", max_length=50)
 
-    created_at = models.DateTimeField(auto_now_add=True)
-    location = models.CharField(max_length=256, default='')
+    def __str__(self):
+        return f"{self.created_at} / {self.location}"
--- introduce/views.py
+++ introduce/views.py
@@ -3,14 +3,7 @@ from .models import AccessLog
 
 
 def introduce_view(request):
-    if request.method == 'POST':
-        create_at = request.POST.get('create_at',None)
-        location = request.POST.get('location',None)
-
-        new_log = AccessLog()
-        new_log.created_at = create_at
-        new_log.location = location
-        new_log.save()
-        
-    elif request.method == 'GET':
-        return render(request, 'index.html')
+    new_log = AccessLog()
+    new_log.location = "index"
+    new_log.save()
+    return render(request, 'index.html')
